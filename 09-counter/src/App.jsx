ff0469213640f9f67433d7109e5017b1cbc79c22
seema-sinhmar <prajapatiseema051@gmail.com>
2026-07-26 15:08:35 +0530

    add 9th file

---
+++ 09-counter/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [num, setnum] = useState(0)
+
+  const incNum = () => {
+    console.log(num)
+    setnum(num + 1)
+  }
+  const decNum = () => {
+    setnum(num - 1)
+  }
+  
+  return (
+    <div className="counter">
+      <h1>{num}</h1>
+        <button onClick={incNum}>Increase</button>
+        <button onClick={decNum}>Decrease</button>
+    </div>
+  );
+};
+
+export default App;
